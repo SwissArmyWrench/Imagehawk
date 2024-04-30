7372601aca824fe6d3f2096cf25b86b247e64574
Nathan <137639586+SwissArmyWrench@users.noreply.github.com>
2024-04-30 16:44:12 -0400

    Build code to parse docker ps images into Image structs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,119 @@
 use std::process::Command;
+
+struct Image<'img> {
+    namespace: &'img str,
+    repo: Option<&'img str>,
+    tag: Option<&'img str>,
+    remote: Remote,
+}
+
+enum Remote {
+    DockerHub,
+    Ghcr,
+    Lscr,
+    Quay,
+}
+
+impl Image<'_> {
+    fn from_str(namestring: &str) -> Image {
+        let parts: Vec<&str> = namestring.split("/").collect();
+
+        // println!("{:?}", &parts);
+
+        if parts.len() == 3 {
+            println!("EXTERNAL. Not yet supported, substituting w/ Pihole image\n");
+            let namespace = parts[1];
+            let remote = if parts[0] == "lscr.io" {
+                Remote::Lscr
+            } else if parts[0] == "ghcr.io" {
+                Remote::Ghcr
+            } else if parts[0] == "quay.io" {
+                Remote::Quay
+            } else {
+                Remote::DockerHub
+            };
+            let repo: Option<&str>;
+            let tag: Option<&str>;
+            if parts[2].contains(":") {
+                // has tag
+                let parts: Vec<&str> = parts[2].split(":").collect();
+                repo = Some(parts[0]);
+                tag = Some(parts[1]);
+
+            } else {
+                repo = Some(parts[0]);
+                tag = None;
+            }
+
+            return Image {
+                namespace,
+                repo, 
+                tag,
+                remote,
+            };
+        } else if parts.len() == 2 {
+            println!("NS AND REPO\n");
+            let namespace = parts[0];
+            let parts: Vec<&str> = parts[1].split(":").collect();
+            let repo = parts[0];
+            let tag: Option<&str> = if parts.len() == 2 {
+                Some(parts[1])
+            } else {
+                None
+            };
+
+            return Image {
+                namespace,
+                repo: Some(repo),
+                tag,
+                remote: Remote::DockerHub,
+            };
+        } else if (parts.len() == 1) && (parts[0].is_empty()) {
+            // do nothing
+        } else if parts.len() == 1 {
+            println!("NO REPO\n");
+            let parts: Vec<&str> = parts[0].split(":").collect();
+            let namespace = parts[0];
+            let repo = None;
+            let tag = Some(parts[1]);
+
+            return Image {
+                namespace,
+                repo,
+                tag,
+                remote: Remote::DockerHub,
+            };
+        };
+
+        // println!("{:?}", &parts);
+
+        println!("NOTHING RETURNED YET\n{:?}", parts);
+        Image {
+            namespace: "error",
+            repo: Some("error"),
+            tag: None,
+            remote: Remote::DockerHub,
+        }
+    }
+
+    fn print(&self) {
+        let parsed_tag = self.tag.unwrap_or("NO TAG");
+        println!(
+            "Namespace: {}\nRepo: {}\nTag: {}\nRemote: {}\n",
+            self.namespace,
+            self.repo.unwrap_or("NO REPO GIVEN"),
+            parsed_tag,
+            match self.remote {
+                Remote::DockerHub => "DockerHub",
+                Remote::Quay => "Quay",
+                Remote::Lscr => "LSCR.io",
+                Remote::Ghcr => "GHCR.io",
+            }
+
+        );
+    }
+}
+
 fn main() {
     // let mut dockerps = Command::new("docker");
     // let mut dockerps = dockerps.arg("ps");
@@ -12,7 +127,20 @@ fn main() {
             .stdout,
     )
     .unwrap();
-    println!("{}", output);
+    // println!("{}", &output);
+    // Working with iterator to remove the quotation marks at each end
+    let mut output_iter: Vec<&str> = output.split('\n').collect();
+    let _ = output_iter.remove(output_iter.len() -1);
+    let output_iter = output_iter.iter().map(|x| x.trim_matches('"'));
+    let output_iter = output_iter.map(|string| Image::from_str(string));
+    // println!("{:?}", &output_iter);
+    let images: Vec<Image> = output_iter.collect();
+    // images.remove(images.len() - 1);
+
+    for image in images {
+        image.print();
+    }
+
     //let parsed: serde_json::Value = serde_json::from_str(&output).unwrap();
     // println!("{}", parsed[0]["Image"]);
 }
